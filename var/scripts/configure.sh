ea1ac686455154737e120975722827f59989374e
strackovski <vladimir.strackovski@gmail.com>
2014-07-22 12:18:52 +0000

    Add error checks for arguments, directory detection, enable auto composer install.

--- var/scripts/configure.sh
+++ var/scripts/configure.sh
@@ -1,14 +1,49 @@
 #!/bin/bash
 # $1 Project name
-BASE=${PWD}
+# Script directory (this file)
+BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ -z "$1" ]; then
-    echo "Undefined project name, aborting."
+    printf "Required argument project-name missing.\n"
+    printf "Usage: ./configure.sh <project-name> [--auto-install]\n"
+    exit 1
 else
-	echo "Configuring project with name $1..."
-	sed -i "s/app-skeleton/$1/" $BASE/composer.json
-	sed -i "s/app-skeleton/$1/" $BASE/.htaccess
-	find $BASE/src/ -type f -exec \
+    cd $BASE
+	cd ../..
+
+	printf "Configuring project with name $1, located in ${PWD##*/} ...\n"
+
+	if [ ! -f composer.json ]; then
+        printf "Required file composer.json not found in project root!\n"
+        exit 1
+    fi
+
+    if [ ! -d src ]; then
+           printf "Directory 'src' not found in project root.\n"
+           printf "Are you missing project sources?\n"
+           exit 1
+    fi
+
+	if [ ! -f .htaccess ]; then
+        printf ".htaccess not found, URL rewriting will not work if it relies on it!\n"
+    else
+        sed -i "s/app-skeleton/$1/" .htaccess
+    fi
+
+    sed -i "s/app-skeleton/$1/" composer.json
+	find src/ -type f -exec \
 	    sed -i "s/PROJECT_NAME/$1/g" {} +
-	echo 'Project configured, run composer install to install dependencies.';
+
+    if [ $2 ]; then
+        if [ $2 == "--auto-install" ]; then
+            printf "Executing composer's installation command...\n"
+            composer install
+            exit 0
+        else
+            printf "Invalid flag $2 \n"
+            exit 1
+        fi
+    else
+        printf "Project configured, run composer install to install dependencies.\n";
+    fi
 fi
