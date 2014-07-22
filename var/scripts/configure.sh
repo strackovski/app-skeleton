edaa0b40befef07c09c0b693f942123eb86d75d4
strackovski <vladimir.strackovski@gmail.com>
2014-07-22 05:43:17 +0000

    Replaced the wrong configuration script.

--- var/scripts/configure.sh
+++ var/scripts/configure.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# $1 Target directory name
+# $1 Project name
 BASE=${PWD}
 
-if [ -d $BASE/$1 ]; then
-	echo "Directory already exists, aborting."
+if [ -z "$1" ]; then
+    echo "Undefined project name, aborting."
 else
-	git clone https://github.com/strackovski/app-skeleton.git $1
 	echo "Configuring project with name $1..."
-	sed -i "s/app-skeleton/$1/" $BASE/$1/composer.json
-	sed -i "s/app-skeleton/$1/" $BASE/$1/.htaccess
-	find $BASE/$1/src/ -type f -exec \
+	sed -i "s/app-skeleton/$1/" $BASE/composer.json
+	sed -i "s/app-skeleton/$1/" $BASE/.htaccess
+	find $BASE/src/ -type f -exec \
 	    sed -i "s/PROJECT_NAME/$1/g" {} +
-	echo 'Project ready!';
+	echo 'Project configured, run composer install to install dependencies.';
 fi
