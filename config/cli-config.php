0402d36119446fe210f5bdbb9b3356b1fb8b8645
strackovski <vladimir.strackovski@gmail.com>
2014-07-22 18:52:29 +0000

    Move to config dir.

---
+++ config/cli-config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * CLI Configuration File for Doctrine
+ */
+
+define('APPPATH', dirname(dirname(__FILE__)) . '/vendor/nv/codeigniter/application/');
+define('BASEPATH', dirname(dirname(__FILE__)) . '/vendor/nv/codeigniter/system/');
+define('FCPATH', dirname(dirname(__FILE__)) . '/');
+define('SRCPATH', dirname(dirname(__FILE__)) . '/src/');
+define('ENVIRONMENT', 'development');
+
+chdir(APPPATH);
+require APPPATH . 'libraries/Doctrine.php';
+
+foreach ($GLOBALS as $helperSetCandidate) {
+    if ($helperSetCandidate instanceof Symfony\Component\Console\Helper\HelperSet) {
+        $helperSet = $helperSetCandidate;
+        break;
+    }
+}
+$doctrine = new Doctrine;
+$em = $doctrine->em;
+
+$helperSet = new Symfony\Component\Console\Helper\HelperSet(array(
+    'db' => new Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
+    'em' => new Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em)
+));
+
+\Doctrine\ORM\Tools\Console\ConsoleRunner::run($helperSet);
