25b33632a3033242cb869d7e3b40664935533467
strackovski <vladimir.strackovski@gmail.com>
2014-07-22 05:24:30 +0000

    Fix variable mess.

--- var/scripts/configure.sh
+++ var/scripts/configure.sh
@@ -8,9 +8,9 @@ if [ -d $BASE/$1 ]; then
 else
 	git clone https://github.com/strackovski/app-skeleton.git $1
 	echo "Configuring project with name $1..."
-	sed -i "s/app-skeleton/$1/" $DIR$1/composer.json
-	sed -i "s/app-skeleton/$1/" $DIR$1/.htaccess
-	find $DIR$1/src/ -type f -exec \
+	sed -i "s/app-skeleton/$1/" $BASE$1/composer.json
+	sed -i "s/app-skeleton/$1/" $BASE$1/.htaccess
+	find $BASE$1/src/ -type f -exec \
 	    sed -i "s/PROJECT_NAME/$1/g" {} +
 	echo 'Project ready!';
 fi
