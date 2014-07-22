e158082d1cfd027e5356c66b9e992ce413fd72b1
strackovski <vladimir.strackovski@gmail.com>
2014-07-22 05:17:55 +0000

    Add configuration script.

---
+++ var/scripts/configure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# $1 Target directory name
+
+if [ -d /srv/samba/share/apache/$1 ]; then
+	echo "Directory already exists, aborting."
+else
+	git clone https://github.com/strackovski/app-skeleton.git $1
+	echo "Configuring project with name $1..."
+	sed -i "s/app-skeleton/$1/" $DIR$1/composer.json
+	sed -i "s/app-skeleton/$1/" $DIR$1/.htaccess
+	find $DIR$1/src/ -type f -exec \
+	    sed -i "s/PROJECT_NAME/$1/g" {} +
+	echo 'Project ready!';
+fi
